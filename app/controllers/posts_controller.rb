bde634165d9f1bd327ea4fe16846f6ad435cd26c
kceron <karemiceron@gmail.com>
2020-08-17 10:33:35 -0400

    form fixes

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -34,7 +34,7 @@ class PostsController < ApplicationController
 
   private
   def post_params
-    params.require(:post).permit[:title, :image, :description, :category]
+    params.permit[:title, :image, :description, :category, :username]
   end
 end
 
