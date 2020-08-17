bff0f4c61bd8692682df3b70458015c2c258941b
kceron <karemiceron@gmail.com>
2020-08-17 16:43:29 -0400

    serializer done

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,18 @@
 class UsersController < ApplicationController
 
+    def index
+        @users = User.all 
+        render json: @users
+    end
+
+    #example login method for login
+    def login
+        @user = User.find_by(name: params[:username, :email])
+        if @user
+            render json: @user 
+        else 
+            render json: {error: "User does not exist."}
+        end
+    end
 
 end
